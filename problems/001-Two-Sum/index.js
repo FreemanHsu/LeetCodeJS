dac58724bd737f0e067d1267772970d8e85bd96b
AndyHsu <andy.xu1994@gmail.com>
2015-05-06 23:17:50 +0800

    002

--- problems/001-Two-Sum/index.js
+++ problems/001-Two-Sum/index.js
@@ -1,22 +1,18 @@
-var twoSum = module.exports = function(numbers, target) {
-  var hash = numbers.reduce(function(prev, item, index) {
-    if (prev[item] === undefined) {
-      prev[item] = index;
-      return prev;
+var twoSum = module.exports = function(nums, target) {
+    var myhash = {};
+    var i;
+    var goal;
+    var index2;
+    var result=[];
+    for (i=0; i<nums.length; i++){
+        myhash[nums[i]] = i;
     }
-    prev[item] = [prev[item], index];
-    return prev;
-  }, {});
-  var i;
-  for (i in hash) {
-    if (hash[i] instanceof Array && i * 2 === target) {
-      return [hash[i][0] + 1, hash[i][1] + 1].sort(function(a, b) {
-        return a - b;
-      });
-    } else if (hash[target - i] !== undefined) {
-      return [hash[i] + 1, hash[target - i] + 1].sort(function(a, b) {
-        return a - b;
-      });
+    for (i=0; i<nums.length; i++){
+        goal = target - nums[i];
+        if (myhash[goal] && myhash[goal]!=i){
+            result.push(i+1, myhash[goal]+1);
+            break;
+        }
     }
-  }
+    return result;
 };
---
+++ problems/002-Add-Two-Numbers/index.js
@@ -0,0 +1,31 @@
+var addTwoNumbers = module.exports = function(listA, listB) {
+  var tmpA = listA;
+  var tmpB = listB;
+  var result, tmpNode, carry, sum;
+  carry = 0;
+  while (tmpA !== null || tmpB !== null) {
+    sum = (tmpA === null ? 0 : tmpA.val) + (tmpB === null ? 0 : tmpB.val) + carry;
+    if (!tmpNode) {
+      tmpNode = result = new ListNode(sum % 10);
+    } else {
+      tmpNode.next = new ListNode(sum % 10);
+      tmpNode = tmpNode.next;
+    }
+    carry = sum > 9 ? 1 : 0;
+    if (tmpA !== null) {
+      tmpA = tmpA.next;
+    }
+    if (tmpB !== null) {
+      tmpB = tmpB.next;
+    }
+  }
+  if (carry === 1) {
+    tmpNode.next = new ListNode(1);
+  }
+  return result;
+};
+
+function ListNode(val) {
+  this.val = val;
+  this.next = null;
+}
